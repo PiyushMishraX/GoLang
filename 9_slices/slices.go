60a07b4195b32ddab4c54903260c908bd4dd7b33
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-09-01 23:58:36 +0530

    Use copy functions , slice method and slice package equal method

--- 9_slices/slices.go
+++ 9_slices/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 // slice --> dynamic array
 // no need for length automatically increased if needed
@@ -53,11 +56,50 @@ func main(){
 	// fmt.Println(len(nums))
 
 
-	nums := []int{}
-	fmt.Println(nums)
-	fmt.Println(cap(nums))
-	fmt.Println(len(nums))
+	// nums := []int{}
+	// nums = append(nums, 1)
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+	// fmt.Println(len(nums))
 
+	// var nums = make([]int, 2 , 5)
+	// nums[0] = 3
+	// nums[1] = 5
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+	// fmt.Println(len(nums))
+
+
+	// // copy functions
+	// var nums = make([]int, 0 , 5)
+	// // var nums2 = make([]int, len(nums))
+	// nums = append(nums, 2)
+	
+	// var nums2 = make([]int, len(nums))
+	// //copy
+	// fmt.Println(nums, nums2)
+	
+	// copy(nums2, nums)
+	// fmt.Println(nums, nums2)
+	
+
+
+	// slice operator
+	// var nums = []int{1,2,3}
+
+	// fmt.Println(nums[0:2]) // return elements from 0 till 2 elements are getten
+
+	// fmt.Println(nums[:1]) // start tilll 1 element
+	// fmt.Println(nums[1:])  // from this index to last
+
+
+	// slice // package
+	var nums1 = []int{1,2,3}
+	var nums2 = []int{1,2,3,4}
+
+	fmt.Println(slices.Equal(nums1, nums2))
+	
+	
 
 
 
