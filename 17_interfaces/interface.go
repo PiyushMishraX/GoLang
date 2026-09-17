6f3ca1a0f7613b78318ec137e764ded8cf55e17c
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-09-17 23:31:48 +0530

    Use function of different struct and opmize it

--- 17_interfaces/interface.go
+++ 17_interfaces/interface.go
@@ -2,60 +2,56 @@ package main
 
 import "fmt"
 
-type paymenter interface {
-	pay(amount float32)
-	refund(amount float32, account string)
-}
+// interfaces
+
+// type payment struct {}
+
+// func (p payment) makePayment(amount float32) {
+// 	// razorpayPaymentGw := razorpay{}
+// 	// // razorpayPaymentGw.pay(amount)
+// 	// stripePaymentGw := stripe{}
+// 	// stripePaymentGw.pay(amount) // we are modifying our code // solid principle
+// 	// Open close principle violated
+// 	// the classes should be open for extension but close for modification 
+// }
 
 type payment struct {
-	gateway paymenter
+	// gateway stripe
+	gateway razorpay
+
 }
 
-// Open close principle
 func (p payment) makePayment(amount float32) {
-	// razorpayPaymentGw := razorpay{}
-	// stripePaymentGw := stripe{}
-	// razorpayPaymentGw.pay(amount)
 	p.gateway.pay(amount)
 }
 
-type razorpay struct{}
+type razorpay struct {}
 
-func (r razorpay) pay(amount float32) {
+func (r razorpay) pay(amount float32){
 	// logic to make payment
-	fmt.Println("making payment using razorpay", amount)
+	fmt.Println("Make payment using rozorpay", amount)
 }
 
-// type stripe struct{}
+// if we have to use stripe for payment
 
-// func (s stripe) pay(amount float32) {
-// 	fmt.Println("making payment using stripe", amount)
-// }
+type stripe struct {}
 
-type fakepayment struct{}
-
-func (f fakepayment) pay(amount float32) {
-	fmt.Println("making payment using fake gateway for testing purpose")
-}
-
-type paypal struct{}
-
-func (p paypal) pay(amount float32) {
-	fmt.Println("making payment using paypal", amount)
-}
-
-func (p paypal) refund(amount float32, account string) {
-
+func (s stripe) pay(amount float32) {
+	fmt.Println("Making payment using stripe", amount)
 }
 
 func main() {
-	// stripePaymentGw := stripe{}
-	// razorpayPaymentGw := razorpay{}
+	// newPayment := payment{} // the struct values are not compulsory , so that might cause some type of mismatch use constructor function to make values compulsory to be defined
+	// newPayment.makePayment(100)
 
-	// fakeGw := fakepayment{}
-	paypalGw := paypal{}
+
+	// stripePaymentGw := stripe{}
+	// newPayment := payment{
+	// 	gateway: stripePaymentGw,
+	// }
+	razorpayPaymentGw := razorpay{}
 	newPayment := payment{
-		gateway: paypalGw,
+		gateway: stripe(razorpayPaymentGw),
 	}
 	newPayment.makePayment(100)
 }
