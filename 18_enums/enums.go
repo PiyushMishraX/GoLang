e9342f0deeef6431a161d80ce4db4b34e8450409
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-09-19 23:38:03 +0530

    Explain the use of enums , implement enums by creating custom type and use enum for order status change

--- 18_enums/enums.go
+++ 18_enums/enums.go
@@ -2,20 +2,57 @@ package main
 
 import "fmt"
 
-// enumerated types
-type OrderStatus string
+// enumrated type
 
+// custom type go
+// type myType string
+
+
+// we create num using custom type
+// type OrderStatus int 
+
+// const (
+// 	Received OrderStatus = iota // iota is preeclared identifier for untyped integer
+// 	Confirmed // 1 // auto increment // we can use string too
+// 	Prepared // 2
+// 	Delivered // 3 
+// )
+
+type OrderStatus string
 const (
-	Received  OrderStatus = "received"
-	Confirmed             = "confirmed"
-	Prepared              = "prepared"
-	Delivered             = "delivered"
+	Received OrderStatus = "received"
+	Confirmed = "confirmed"
+	Prepared = "prepared"
+	Delivered = "delivered"
 )
 
-func changeOrderStatus(status OrderStatus) {
-	fmt.Println("changing order status to", status)
+
+// func changeOrderStatus(status string) {
+// 	fmt.Println("Changing order status to", status)
+// }
+
+func changeOrderStatus1(status OrderStatus) {
+	fmt.Println("Changing order status to", status)
 }
 
 func main() {
-	changeOrderStatus(Prepared)
+//  changeOrderStatus("received")
+//  changeOrderStatus("confirmed")
+ // here might have typo , and there we have to use those for multiple time
+ // we have to write everywhere for normal name change too
+ // order status type can be vast but limited 
+ // go do not have inbuilt enum , we implement using const and type
+
+ // integer
+//  changeOrderStatus1(Received)
+//  changeOrderStatus1(Confirmed)
+//  changeOrderStatus1(Prepared)
+
+// string
+ changeOrderStatus1(Received)
+ changeOrderStatus1(Confirmed)
+ changeOrderStatus1(Prepared)
+ 
+ 
+
 }
