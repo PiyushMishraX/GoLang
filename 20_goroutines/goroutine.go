e5c42fb89deed4008c3c296a26ccfae3bfd74c94
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-09-21 23:56:16 +0530

    Use and explain go routine, create multiple light weight threads and use goroutine in an inline function

--- 20_goroutines/goroutine.go
+++ 20_goroutines/goroutine.go
@@ -1,22 +1,51 @@
+// goroutines
+
 package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
-func task(id int, w *sync.WaitGroup) {
-	defer w.Done()
-	fmt.Println("doing task", id)
+
+func task(id int) {
+	fmt.Println("Doing task", id)
+
 }
-
 func main() {
-	var wg sync.WaitGroup
 
-	for i := 0; i <= 10; i++ {
-		wg.Add(1)
-		go task(i, &wg)
+	// for i := 0; i<=10; i++ {
+	// 	task(i)
+	// } // have a order 0 to 10
+
+	// but we want to run them parallely
+	// for i := 0; i<=10; i++ {
+	// 	go task(i) // runs in lightweight thread(goroutine) concorrently
+	// 	// schedular scheules them seperately without blocking while the main functions ends seperately( exist)
+	// }  // very fast compared
+
+	// go fn()
+
+
+
+
+	for i := 0; i<=10; i++ {
+		// go task(i)
+
+		// inline fn
+		// go func () {
+		// 	fmt.Println(i) // i closure
+		// } ()
+
+		// better practice , receive the i even if closure
+		go func (i int) {
+			fmt.Println(i)
+		} (i)
 	}
 
-	wg.Wait()
+
+	// go routine --> we can write background workers ,do cpu intensive work concurrently
+
+	time.Sleep(time.Second * 2) // 2 second main holded 
+
 }
